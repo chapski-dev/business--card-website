447d871b3da13fcb0b70c7e4cfb01f6508774ad3
Chapski <alexey.chapski@gmail.com>
2022-04-27 18:46:00 +0300

    Auto-fix by ESLint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { Routes, Route } from "react-router-dom";
-import FuniroPage from "./pages/FuniroPage/src/index";
-import HomePage from "./pages/HomePage/index";
-import FigmaLandPage from "./pages/FigmaLandPage/index";
-import ItStudioPage from "./pages/ItStudioPage/src/index";
-import AddCardPage from "./pages/AddCardPage/index";
-import { Provider } from "react-redux";
-import store from "./store";
-import { ErrorPage } from "./pages/ErrorPage";
+import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import FuniroPage from './pages/FuniroPage/src/index';
+import HomePage from './pages/HomePage/index';
+import FigmaLandPage from './pages/FigmaLandPage/index';
+import ItStudioPage from './pages/ItStudioPage/src/index';
+import AddCardPage from './pages/AddCardPage/index';
+import store from './store';
+import { ErrorPage } from './pages/ErrorPage';
 
-const App = () => (
-  <Provider store={store}>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/funiro" element={<FuniroPage />} />
-      <Route path="/figma-land" element={<FigmaLandPage />} />
-      <Route path="/it-studio-page" element={<ItStudioPage />} />
-      <Route path="/card" element={<AddCardPage />} />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
-  </Provider>
-);
+function App() {
+  return (
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/funiro" element={<FuniroPage />} />
+        <Route path="/figma-land" element={<FigmaLandPage />} />
+        <Route path="/it-studio-page" element={<ItStudioPage />} />
+        <Route path="/card" element={<AddCardPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Provider>
+  );
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "antd/dist/antd.css";
-import "./index.scss";
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import 'antd/dist/antd.css';
+import './index.scss';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
